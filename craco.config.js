265292bd06ab5a8bf5d9369a11292f5a792eae4b
Maksim Mashko <maks.mashko@gmail.com>
2024-07-09 14:50:00 +0200

    fix: craco config

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 module.exports = {
   webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.output.publicPath = process.env.PUBLIC_URL || "/";
+      return webpackConfig;
+    },
     alias: {
       "@": path.resolve(__dirname, "src/"),
     },
